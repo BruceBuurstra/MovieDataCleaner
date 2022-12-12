ac7588d0eea4bca97e9b7792ee075290b46f784b
Bruce Buurstra <buursbru@mail.gvsu.edu>
2022-12-11 22:04:48 -0500

    package documentation

---
+++ R/.R
@@ -0,0 +1,15 @@
+#' MovieDataCleaner
+#'
+#' @description This package is able to impute missing values into datasets with large amounts
+#'  of missing values using both knn imputation and NMF imputation.
+#'
+#' @author Bruce Buurstra
+#'
+#'
+#'
+#'
+#'
+#'
+#'
+#'
+#'
