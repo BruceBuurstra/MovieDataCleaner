1948ba3274e4926c70ceef334e3c45176e1abf40
Bruce Buurstra <buursbru@mail.gvsu.edu>
2022-12-11 21:46:16 -0500

    fixed cran error and nmf function, working on vignette

--- R/run_nmf_imputation.R
+++ R/run_nmf_imputation.R
@@ -7,12 +7,14 @@
 #' @return Returns the dataset using NMF of rank \code{z} to impute missing values
 #' @examples
 #'
-#'  # returns our movie ratings dataset with missing values imputed from an NMF of rank 3
+#' run_nmf_imputation(3, new_data) # returns our movie ratings dataset with missing values imputed from an NMF of rank 3
 #'
 #' @export
 
 # Reducing the dimension of dataset using nmf
 run_nmf_imputation <- function(z, data) {
+  options(RcppML.verbose = FALSE)
+  options(RcppML.threads = 0)
   data_matrix <- data
   data_matrix[is.na(data_matrix)] <- 0
   data_matrix <- data.matrix(data_matrix)
@@ -24,4 +26,3 @@ run_nmf_imputation <- function(z, data) {
   return(data_nmf_imputed)
 }
 
-#run_nmf_imputation(3, new_data)
