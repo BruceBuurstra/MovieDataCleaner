ee5147c5bf9f44ff6176284d2b1c1c25fd3b047b
zdebruine <zacharydebruine@gmail.com>
2022-12-13 14:34:12 -0500

    update round_to_movie_rating

--- R/round_to_movie_rating.R
+++ R/round_to_movie_rating.R
@@ -12,28 +12,5 @@
 
 # Rounding the value to movie ratings
 round_to_movie_rating <- function(x) {
-  if (x < 0.25) {
-    x <- 0
-  } else if (x < 0.75) {
-    x <- 0.5
-  } else if (x < 1.25) {
-    x <- 1
-  } else if (x < 1.75) {
-    x <- 1.5
-  } else if (x < 2.25) {
-    x <- 2
-  } else if (x < 2.75) {
-    x <- 2.5
-  } else if (x < 3.25) {
-    x <- 3
-  } else if (x < 3.75) {
-    x <- 3.5
-  } else if (x < 4.25) {
-    x <- 4
-  } else if (x < 4.75) {
-    x <- 4.5
-  } else {
-    x <- 5
-  }
-  return(x)
+  ifelse(x > 5, 5, round(x))
 }
